a3a2d3870726f7548fd906ac190b32379c7efb1d
guo <904004593@qq.com>
2022-04-16 00:53:21 +0800

    完成

--- src/main/java/com/warehouse/WarehouseApplication.java
+++ src/main/java/com/warehouse/WarehouseApplication.java
@@ -1,11 +1,13 @@
 package com.warehouse;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.util.unit.DataSize;
 
 import javax.servlet.MultipartConfigElement;
@@ -15,13 +17,17 @@ import javax.servlet.MultipartConfigElement;
  * @creed: 少壮不努力, 以后卡卡西
  * @Date 2022/4/16 0:13
  */
+@Slf4j
 @Configuration
 @SpringBootApplication
 @MapperScan(basePackages = {"com.warehouse.*.mapper"})
 public class WarehouseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WarehouseApplication.class, args);
+		SpringApplication app=new SpringApplication(WarehouseApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		log.info("启动成功！！地址: \thttp://localhost:{}", env.getProperty("server.port"));
+
 	}
 
 	@Bean
